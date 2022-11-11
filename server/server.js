0cb98861da48dc6a56481c1cc421bb2e8e342a74
youngkyu <toctoce@naver.com>
2022-11-11 14:16:15 +0900

    yk

--- server/server.js
+++ server/server.js
@@ -4,5 +4,5 @@ const test = require('./Router/test.js');
 
 app.use('/', test);
 
-const port = 5000; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
+const port = 3000; //React가 3000번 포트를 사용하기 때문에 node 서버가 사용할 포트넘버는 다른 넘버로 지정해준다.
 app.listen(port, () => { console.log(`Listening on port ${port}`) });
